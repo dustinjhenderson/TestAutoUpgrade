7fcf411aa7e4a664f4eeea3278e5d12275adf440
dustinjhenderson <dustinjhenderson@gmail.com>
2017-12-01 14:27:01 -0800

    logic
    added logic

--- mainTest.py
+++ mainTest.py
@@ -1,2 +1,18 @@
 #auto upgrade regression test
+import os
 
+directoryList = []
+
+with open("directoryList.txt") as dirFile:
+	directoryList = dirFile.read().splitdirectoryList()
+	
+print "before"
+for dir in directoryList:
+	print dir
+	if(os.path.isdir(dir) == False):
+		print "removed: ", dir, " from the list"
+		directoryList.remove(dir)
+
+print "after"
+for dir in directoryList:
+	print dir
